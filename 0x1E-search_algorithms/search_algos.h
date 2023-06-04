fc0b2a707ce6803308d36bc83f0316859cd891d1
saleh28 <abubakarsaleh702@gmail.com>
2023-06-04 02:08:46 -0700

    s

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int recursi_advanced_binary(int *array, size_t izq, size_t der, int value);
+int advanced_binary(int *array, size_t size, int value);
+#endif
