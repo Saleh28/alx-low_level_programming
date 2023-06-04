3af18ffe7f09afbd03adc3e2f3a08a7ae2256c2c
saleh28 <abubakarsaleh702@gmail.com>
2023-06-04 01:53:26 -0700

    t0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - linear_search
+ * @array: pointers
+ * @size: size
+ * @value: value
+ * Return: macht
+ * Description: Fuction
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t index;
+
+	if (array != NULL)
+		for (index = 0; index < size; index++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+			if (array[index] == value)
+			{
+				return (index);
+			}
+		}
+	return (-1);
+}
